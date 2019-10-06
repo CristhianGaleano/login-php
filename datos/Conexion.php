f0c40959d07a77814a05e3a41f8f4f980d9adf9b
Cristhian Alexis Galeano Ruiz <titiruizah@gmail.com>
2019-10-06 14:01:28 -0500

    Se generan las clases de conexion y la primera entidad -usuario- todo ok, nada en index

---
+++ datos/Conexion.php
@@ -0,0 +1,31 @@
+<?php 
+ 
+ /**
+  * 
+  */
+ class Conexion 
+ {
+ 	
+
+ 	/**
+ 	 * Conexion a la BD 
+ 	 * @return [PDO] [Un objeto de tipo PDO]
+ 	 */
+ 	public static function conectar(){
+
+ 		try {
+ 			
+ 			$cn = new PDO("mysql:host=localhost;dbname=login-php","root","1088264375C");
+ 			return $cn;
+
+ 		} catch (PDOException $e) {
+ 			die($e->getMessage());
+ 		}
+ 	}
+
+ }
+
+
+ Conexion::conectar();
+
+ ?>
