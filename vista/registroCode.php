d8e299213be248811d6d3acf81b628dc397ec6d3
Cristhian Alexis Galeano Ruiz <titiruizah@gmail.com>
2019-10-25 10:27:11 -0500

    Se crea registroCode

--- vista/registroCode.php
+++ vista/registroCode.php
@@ -15,26 +15,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	if (isset($_POST['txtNombre'])  && isset($_POST['txtUsuario']) && isset($_POST['txtPassword']) && isset($_POST['txtEmail']) ) {
 	
-	$txtUsuario = validar_campo($_POST['txtNombre']);
+	$txtNombre = validar_campo($_POST['txtNombre']);
 	$txtUsuario = validar_campo($_POST['txtUsuario']);
 	$txtPassword = validar_campo($_POST['txtPassword']);
-	$txtPassword = validar_campo($_POST['txtEmail']);
+	$txtEmail = validar_campo($_POST['txtEmail']);
+	$txtPrivilegio = 2;
 
 
 	$resultado = array('estado' => 'true' );
 
-	if (UsuarioControlador::login( $txtUsuario,$txtPassword )) {
+	if (UsuarioControlador::registrar($txtNombre,$txtUsuario,$txtEmail,$txtPassword,$txtPrivilegio)) {
 	#echo 'Disque true';
 		$usuario = UsuarioControlador::get_usuario($txtUsuario,$txtPassword);
 
-		$_SESSION['usuario'] = array(
-			'id' => $usuario->getId(),
-			'nombre' => $usuario->getNombre(),
-			'usuario' => $usuario->getUsuario(),
-			'email' => $usuario->getEmail(),
-			'privilegio' => $usuario->getPrivilegio(),
-			'fecha_registro' => $usuario->getFecha_registro()
-		);
+		
 		return print( json_encode( $resultado ) );
 	}else{
 	#	echo 'Disque falso';
